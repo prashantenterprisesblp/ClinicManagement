deb3a56a1b5e83756cbb3eef875f94ece668edbd
prashantenterprisesblp <prashantenterprisesblp@gmail.com>
2025-05-22 07:39:14 +0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../config/database.php';
+
+// Destroy session and redirect to login
+session_destroy();
+
+// Clear session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Redirect to login page
+header('Location: login.php');
+exit;
+?>
